e46b996393333819478f782affcb0556edc31a62
Leon Lucic <llucic@vub.hr>
2024-06-05 20:26:38 +0200

    dodana klasa za rad sa tablicom cities

---
+++ Cities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking
+{
+    public class Cities{
+        public Cities() { 
+            db = new BookingDB();
+        }
+
+        public IEnumerable<City> All(){
+            return 
+                from city in db.Cities
+                orderby city.City1
+                select city;
+        }
+        private BookingDB db;
+    }
+}
