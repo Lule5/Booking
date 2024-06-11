07d207a12828fb97a39875211ffe0bdd2ba0f0da
Leon Lucic <llucic@vub.hr>
2024-06-11 17:22:51 +0200

    dodana forma za rezervacije

--- Reservations/ReservationForm.Designer.cs
+++ Reservations/ReservationForm.Designer.cs
@@ -24,20 +24,203 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReservationForm));
+            this.lblApartmentName = new System.Windows.Forms.Label();
+            this.pbApartmentPicture = new System.Windows.Forms.PictureBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblApartmentAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblApartmentCity = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dtpArrivalDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpDepartureDate = new System.Windows.Forms.DateTimePicker();
+            this.lblArrivalDate = new System.Windows.Forms.Label();
+            this.lblDepartureDate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnReserve = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbApartmentPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblApartmentName
+            // 
+            this.lblApartmentName.AutoSize = true;
+            this.lblApartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblApartmentName.Location = new System.Drawing.Point(305, 28);
+            this.lblApartmentName.Name = "lblApartmentName";
+            this.lblApartmentName.Size = new System.Drawing.Size(193, 32);
+            this.lblApartmentName.TabIndex = 0;
+            this.lblApartmentName.Text = "sadasdsadsa";
+            // 
+            // pbApartmentPicture
+            // 
+            this.pbApartmentPicture.Location = new System.Drawing.Point(37, 28);
+            this.pbApartmentPicture.Name = "pbApartmentPicture";
+            this.pbApartmentPicture.Size = new System.Drawing.Size(212, 171);
+            this.pbApartmentPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbApartmentPicture.TabIndex = 1;
+            this.pbApartmentPicture.TabStop = false;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAddress.Location = new System.Drawing.Point(307, 86);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(81, 22);
+            this.lblAddress.TabIndex = 2;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblApartmentAddress
+            // 
+            this.lblApartmentAddress.AutoSize = true;
+            this.lblApartmentAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblApartmentAddress.Location = new System.Drawing.Point(406, 86);
+            this.lblApartmentAddress.Name = "lblApartmentAddress";
+            this.lblApartmentAddress.Size = new System.Drawing.Size(82, 22);
+            this.lblApartmentAddress.TabIndex = 3;
+            this.lblApartmentAddress.Text = "sdafdsds";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCity.Location = new System.Drawing.Point(307, 141);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(46, 22);
+            this.lblCity.TabIndex = 4;
+            this.lblCity.Text = "City:";
+            // 
+            // lblApartmentCity
+            // 
+            this.lblApartmentCity.AutoSize = true;
+            this.lblApartmentCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblApartmentCity.Location = new System.Drawing.Point(406, 141);
+            this.lblApartmentCity.Name = "lblApartmentCity";
+            this.lblApartmentCity.Size = new System.Drawing.Size(82, 22);
+            this.lblApartmentCity.TabIndex = 5;
+            this.lblApartmentCity.Text = "sdafdsds";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Booking.Properties.Resources.logo1;
+            this.pictureBox1.Location = new System.Drawing.Point(653, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dtpArrivalDate
+            // 
+            this.dtpArrivalDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpArrivalDate.Location = new System.Drawing.Point(264, 275);
+            this.dtpArrivalDate.Name = "dtpArrivalDate";
+            this.dtpArrivalDate.Size = new System.Drawing.Size(124, 22);
+            this.dtpArrivalDate.TabIndex = 7;
+            // 
+            // dtpDepartureDate
+            // 
+            this.dtpDepartureDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDepartureDate.Location = new System.Drawing.Point(604, 275);
+            this.dtpDepartureDate.Name = "dtpDepartureDate";
+            this.dtpDepartureDate.Size = new System.Drawing.Size(124, 22);
+            this.dtpDepartureDate.TabIndex = 8;
+            // 
+            // lblArrivalDate
+            // 
+            this.lblArrivalDate.AutoSize = true;
+            this.lblArrivalDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblArrivalDate.Location = new System.Drawing.Point(260, 231);
+            this.lblArrivalDate.Name = "lblArrivalDate";
+            this.lblArrivalDate.Size = new System.Drawing.Size(106, 22);
+            this.lblArrivalDate.TabIndex = 9;
+            this.lblArrivalDate.Text = "Arrival date:";
+            // 
+            // lblDepartureDate
+            // 
+            this.lblDepartureDate.AutoSize = true;
+            this.lblDepartureDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblDepartureDate.Location = new System.Drawing.Point(600, 231);
+            this.lblDepartureDate.Name = "lblDepartureDate";
+            this.lblDepartureDate.Size = new System.Drawing.Size(135, 22);
+            this.lblDepartureDate.TabIndex = 10;
+            this.lblDepartureDate.Text = "Departure date:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(306, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Price:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPrice.Location = new System.Drawing.Point(374, 393);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(108, 25);
+            this.lblPrice.TabIndex = 12;
+            this.lblPrice.Text = "asdsadsad";
+            // 
+            // btnReserve
+            // 
+            this.btnReserve.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnReserve.Location = new System.Drawing.Point(532, 390);
+            this.btnReserve.Name = "btnReserve";
+            this.btnReserve.Size = new System.Drawing.Size(84, 37);
+            this.btnReserve.TabIndex = 13;
+            this.btnReserve.Text = "Reserve";
+            this.btnReserve.UseVisualStyleBackColor = true;
             // 
             // ReservationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(811, 468);
+            this.Controls.Add(this.btnReserve);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDepartureDate);
+            this.Controls.Add(this.lblArrivalDate);
+            this.Controls.Add(this.dtpDepartureDate);
+            this.Controls.Add(this.dtpArrivalDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblApartmentCity);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblApartmentAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.pbApartmentPicture);
+            this.Controls.Add(this.lblApartmentName);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReservationForm";
             this.Text = "Booking";
+            ((System.ComponentModel.ISupportInitialize)(this.pbApartmentPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblApartmentName;
+        private System.Windows.Forms.PictureBox pbApartmentPicture;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblApartmentAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblApartmentCity;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtpArrivalDate;
+        private System.Windows.Forms.DateTimePicker dtpDepartureDate;
+        private System.Windows.Forms.Label lblArrivalDate;
+        private System.Windows.Forms.Label lblDepartureDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnReserve;
     }
 }
